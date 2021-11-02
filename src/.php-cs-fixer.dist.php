1785958f77c014ac6cf3bb6b4f112f2dc16651ab
milepanic <mile.panic96@gmail.com>
2021-11-02 15:47:56 +0100

    Upgrade code to support php-cs v3, add .php-cs-fixer.dist.php file

--- src/.php-cs-fixer.dist.php
+++ src/.php-cs-fixer.dist.php
@@ -1,13 +1,25 @@
 <?php
 
-namespace VertexIT;
-
-use PhpCsFixer\Config;
 use PhpCsFixer\Finder;
+use PhpCsFixer\Config;
+
+$finder = Finder::create()
+    ->in(
+        [
+            __DIR__ . '/app',
+            __DIR__ . '/config',
+            __DIR__ . '/database',
+            __DIR__ . '/routes',
+            __DIR__ . '/tests',
+        ]
+    );
+
+
+return styles($finder);
 
 function styles(Finder $finder, array $rules = []): Config {
     $rules = array_merge(require __DIR__.'/rules.php', $rules);
-    
+
     $config = new Config();
 
     return $config
