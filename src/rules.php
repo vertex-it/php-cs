5fd185e0e324f5a9c8f96a14e18c2a64e937bbd1
milepanic <mile.panic96@gmail.com>
2021-11-03 13:07:36 +0100

    Add new rules

--- src/rules.php
+++ src/rules.php
@@ -1,25 +1,26 @@
 <?php
 
-// last reviewed: v1.0.0
+// last reviewed: v1.1.4
 return [
     'array_syntax' => ['syntax' => 'short'],
+    'array_indentation' => true,
+    'align_multiline_comment' => true,
     'binary_operator_spaces' => [
         'default' => 'single_space',
-        'operators' => ['=>' => null]
     ],
     'blank_line_after_namespace' => true,
     'blank_line_after_opening_tag' => true,
     'blank_line_before_statement' => [
-        'statements' => ['return']
+        'statements' => ['return'],
     ],
     'braces' => true,
     'cast_spaces' => true,
     'class_attributes_separation' => [
-        'elements' => ['method' => 'one']
+        'elements' => ['method' => 'one', 'trait_import' => 'none'],
     ],
     'class_definition' => true,
     'concat_space' => [
-        'spacing' => 'one'
+        'spacing' => 'one',
     ],
     'declare_equal_normalize' => true,
     'elseif' => true,
@@ -35,9 +36,10 @@ return [
     'linebreak_after_opening_tag' => true,
     'line_ending' => true,
     'lowercase_cast' => true,
+    'native_function_type_declaration_casing' => true,
     'constant_case' => true,
     'lowercase_keywords' => true,
-    'lowercase_static_reference' => true,    
+    'lowercase_static_reference' => true,
     'magic_method_casing' => true,
     'magic_constant_casing' => true,
     'method_argument_space' => true,
@@ -48,8 +50,7 @@ return [
             'extra',
             'throw',
             'use',
-            'use_trait',
-        ]
+        ],
     ],
     'no_blank_lines_after_class_opening' => true,
     'no_blank_lines_after_phpdoc' => true,
@@ -59,30 +60,35 @@ return [
     'no_leading_import_slash' => true,
     'no_leading_namespace_whitespace' => true,
     'no_mixed_echo_print' => [
-        'use' => 'echo'
+        'use' => 'echo',
     ],
     'no_multiline_whitespace_around_double_arrow' => true,
+    'no_null_property_initialization' => true,
     'multiline_whitespace_before_semicolons' => [
-        'strategy' => 'no_multi_line'
+        'strategy' => 'no_multi_line',
     ],
     'no_short_bool_cast' => true,
     'no_singleline_whitespace_before_semicolons' => true,
     'no_spaces_after_function_name' => true,
     'no_spaces_around_offset' => true,
     'no_spaces_inside_parenthesis' => true,
+    'no_superfluous_elseif' => true,
     'no_trailing_comma_in_list_call' => true,
     'no_trailing_comma_in_singleline_array' => true,
     'no_trailing_whitespace' => true,
+    'return_assignment' => true,
     'no_trailing_whitespace_in_comment' => true,
     'no_unneeded_control_parentheses' => true,
     'no_unreachable_default_argument_value' => true,
     'no_useless_return' => true,
+    'no_empty_comment' => true,
     'no_whitespace_before_comma_in_array' => true,
     'no_whitespace_in_blank_line' => true,
     'normalize_index_brace' => true,
-    'not_operator_with_successor_space' => false,
+    'not_operator_with_successor_space' => true,
     'object_operator_without_whitespace' => true,
     'ordered_imports' => ['sort_algorithm' => 'alpha'],
+    'ordered_class_elements' => true,
     'phpdoc_indent' => true,
     'general_phpdoc_tag_rename' => true,
     'phpdoc_inline_tag_normalizer' => true,
@@ -107,11 +113,13 @@ return [
     'single_import_per_statement' => true,
     'single_line_after_imports' => true,
     'single_line_comment_style' => [
-        'comment_types' => ['hash']
+        'comment_types' => ['hash'],
     ],
     'single_quote' => true,
+    'single_trait_insert_per_statement' => true,
     'space_after_semicolon' => true,
     'standardize_not_equals' => true,
+    'simple_to_complex_string_variable' => true,
     'switch_case_semicolon_to_colon' => true,
     'switch_case_space' => true,
     'ternary_operator_spaces' => true,
@@ -119,7 +127,7 @@ return [
     'trim_array_spaces' => true,
     'unary_operator_spaces' => true,
     'visibility_required' => [
-        'elements' => ['method', 'property']
+        'elements' => ['method', 'property', 'const'],
     ],
     'whitespace_after_comma_in_array' => true,
     'no_unused_imports' => true,
